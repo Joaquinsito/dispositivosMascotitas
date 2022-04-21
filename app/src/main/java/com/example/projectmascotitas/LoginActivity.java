5f0c50dae74e770b12c277c581e9f56539f2ba98
Joaquin <joaquin.zatarain@cceo.com.mx>
2022-04-21 13:11:56 -0500

    Se inicio con el proyecto, se puso la splash screen

---
+++ app/src/main/java/com/example/projectmascotitas/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.projectmascotitas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
