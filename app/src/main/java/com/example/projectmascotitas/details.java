0e19bb23c49cd7e1f1ba78db1ff02c23ef67c32a
Joaquin <joaquin.zatarain@cceo.com.mx>
2022-06-05 10:41:25 -0500

    Se agrego, howto, rating, contact

--- app/src/main/java/com/example/projectmascotitas/details.java
+++ app/src/main/java/com/example/projectmascotitas/details.java
@@ -2,6 +2,7 @@ package com.example.projectmascotitas;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -17,9 +18,11 @@ public class details extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         final TextView txtResult = findViewById(R.id.nameproduct);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         Intent getData = getIntent();
         String textoRecibido = getData.getStringExtra(index.EXTRA_MESSAGE);
